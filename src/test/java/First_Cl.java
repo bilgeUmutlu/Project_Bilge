cd2fc7b2e06401a6940daf5459d721dee3b2c940
suatkeskin <suatk1602@gmail.com>
2021-12-27 23:24:17 +0000

    late

--- src/test/java/First_Cl.java
+++ src/test/java/First_Cl.java
@@ -1,8 +1,8 @@
 public class First_Cl {
     public static void main(String[] args) {
-//gjsdkghdkg
+
 
         System.out.println("I added some files here");
-        System.out.println("also here");
+        System.out.println(" suat");
     }
 }
