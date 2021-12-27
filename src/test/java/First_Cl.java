42b929da5aa653d943f073ce511fb1d23640afe2
cemre <mrsscose@gmail.com>
2021-12-27 22:03:22 +0000

    first commit cemre1 i added 3 classes

--- src/test/java/First_Cl.java
+++ src/test/java/First_Cl.java
@@ -1,5 +1,8 @@
 public class First_Cl {
     public static void main(String[] args) {
 
+        System.out.println("practice first");
+
+
     }
 }
---
+++ src/test/java/cemre1.java
@@ -0,0 +1,6 @@
+public class cemre1 {
+
+    public static void main(String[] args) {
+        System.out.println("cemre 1");
+    }
+}
